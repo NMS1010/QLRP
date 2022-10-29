82f720c55a578783f9345834251597c53895d6bb
Tran Minh Man <96435164+TranMinhMan040702@users.noreply.github.com>
2022-10-29 15:29:15 +0700

    UI Bill

---
+++ QuanLyRapPhim/GUI/frm_bill.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace QuanLyRapPhim.GUI
+{
+    partial class frm_bill
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_filmName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lb_time = new System.Windows.Forms.Label();
+            this.lb_date = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lb_roomSeat = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lb_price = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btn_confirm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cinestar SDM";
+            // 
+            // lb_filmName
+            // 
+            this.lb_filmName.AutoSize = true;
+            this.lb_filmName.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_filmName.Location = new System.Drawing.Point(67, 51);
+            this.lb_filmName.Name = "lb_filmName";
+            this.lb_filmName.Size = new System.Drawing.Size(189, 31);
+            this.lb_filmName.TabIndex = 1;
+            this.lb_filmName.Text = "No Way Home";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "SUẤT/TIME:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(216, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "NGÀY/DATE:";
+            // 
+            // lb_time
+            // 
+            this.lb_time.AutoSize = true;
+            this.lb_time.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_time.Location = new System.Drawing.Point(21, 148);
+            this.lb_time.Name = "lb_time";
+            this.lb_time.Size = new System.Drawing.Size(65, 31);
+            this.lb_time.TabIndex = 4;
+            this.lb_time.Text = "time";
+            // 
+            // lb_date
+            // 
+            this.lb_date.AutoSize = true;
+            this.lb_date.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_date.Location = new System.Drawing.Point(228, 148);
+            this.lb_date.Name = "lb_date";
+            this.lb_date.Size = new System.Drawing.Size(67, 31);
+            this.lb_date.TabIndex = 5;
+            this.lb_date.Text = "date";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(15, 194);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(171, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "PHÒNG/HALL, GHẾ/SEAT:";
+            // 
+            // lb_roomSeat
+            // 
+            this.lb_roomSeat.AutoSize = true;
+            this.lb_roomSeat.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_roomSeat.Location = new System.Drawing.Point(21, 225);
+            this.lb_roomSeat.Name = "lb_roomSeat";
+            this.lb_roomSeat.Size = new System.Drawing.Size(102, 31);
+            this.lb_roomSeat.TabIndex = 7;
+            this.lb_roomSeat.Text = "05/R03";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(15, 275);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(110, 16);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "GIÁ TIỀN/PRICE:";
+            // 
+            // lb_price
+            // 
+            this.lb_price.AutoSize = true;
+            this.lb_price.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_price.Location = new System.Drawing.Point(143, 262);
+            this.lb_price.Name = "lb_price";
+            this.lb_price.Size = new System.Drawing.Size(162, 31);
+            this.lb_price.TabIndex = 9;
+            this.lb_price.Text = "95.000 VNĐ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(51, 317);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(228, 13);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "(Đã bao gồm 5% thuế VAT / 5% VAT included)";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(77, 339);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(162, 13);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "Vé miễn đổi trả / Non-refundable";
+            // 
+            // btn_confirm
+            // 
+            this.btn_confirm.BackColor = System.Drawing.Color.Lime;
+            this.btn_confirm.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_confirm.Location = new System.Drawing.Point(90, 362);
+            this.btn_confirm.Name = "btn_confirm";
+            this.btn_confirm.Size = new System.Drawing.Size(136, 30);
+            this.btn_confirm.TabIndex = 12;
+            this.btn_confirm.Text = "Xác nhận / Confirm";
+            this.btn_confirm.UseVisualStyleBackColor = false;
+            // 
+            // frm_bill
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(316, 404);
+            this.ControlBox = false;
+            this.Controls.Add(this.btn_confirm);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lb_price);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lb_roomSeat);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lb_date);
+            this.Controls.Add(this.lb_time);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lb_filmName);
+            this.Controls.Add(this.label1);
+            this.Name = "frm_bill";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hóa Đơn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_filmName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lb_time;
+        private System.Windows.Forms.Label lb_date;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lb_roomSeat;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lb_price;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btn_confirm;
+    }
+}
